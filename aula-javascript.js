ab4394785ff56c91b4b45dfa6971c881af852616
94V1 <0000716556@senaimgaluno.com.br>
2026-06-16 23:34:51 -0300

    Add files via upload

---
+++ aula-javascript.js
@@ -0,0 +1,10 @@
+let n = Number (prompt("Digite um número: "));
+if (n < 0) {
+console.log (`${n} é um número negativo`)
+}
+else if (n > 0){
+console.log (`${n} é um número positivo`)
+}
+else {
+console.log (`O número é zero`)
+}
